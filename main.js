d845527ca929d705f8a375c1009edf9ad2d1c039
jammus <jammus@gmail.com>
2012-09-23 14:21:34 +0100

    Add underscore.

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ requirejs.config({
     paths: {
         jquery: '../lib/jquery/jquery-require',
         three: '../lib/three/three-require',
-        text: '../lib/require/text'
+        text: '../lib/require/text',
+        underscore: '../lib/underscore/underscore-require'
     }
 });
 
-requirejs(["lib/jquery/jquery.min.js", "lib/three/three.min.js"], function() {
+requirejs(["lib/jquery/jquery.min.js", "lib/three/three.min.js", "lib/underscore/underscore-min.js"], function() {
     jQuery('[data-require]').each(function(index, element) {
         requirejs([$(element).data('require')], function() {
         });
